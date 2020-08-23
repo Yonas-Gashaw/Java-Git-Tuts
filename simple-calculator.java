48059344be32ebe7b8693f9e02b9e4e87b667e7e
Yonas-Gashaw <69680376+Yonas-Gashaw@users.noreply.github.com>
2020-08-23 21:55:00 +0300

    Create simple-calculator.java

---
+++ simple-calculator.java
@@ -0,0 +1,21 @@
+
+import java.util.Scanner;
+
+public class Simple-Calculator{
+
+  private int num1;
+  private int num2;
+  
+  public int sum(int a, int b){
+    this.num1 = a;
+    this.num2 = b;
+    
+    return (this.num1 + this.num2);
+  }
+  
+  public static void main(String[] args){
+       // call to sum function
+       int total = sum(11, 20);
+       System.out.println("The sum is: " + total);
+  }
+}
