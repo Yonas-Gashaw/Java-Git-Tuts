d8a6e56931da5e7b8e1fc9cf9bba8bb81621b3b0
Yonas-Gashaw <69680376+Yonas-Gashaw@users.noreply.github.com>
2020-08-23 22:01:00 +0300

    Update simple-calculator.java

--- simple-calculator.java
+++ simple-calculator.java
@@ -13,9 +13,20 @@ public class Simple-Calculator{
     return (this.num1 + this.num2);
   }
   
+  public int product(int a, int b){
+    this.num1 = a;
+    this.num2 = b;
+    
+    return (this.num1 * this.num2);
+  }
+  
   public static void main(String[] args){
        // call to sum function
        int total = sum(11, 20);
        System.out.println("The sum is: " + total);
+    
+      // call to product function
+       int prod = product(22, 33);
+       System.out.println("The product is: " + prod);
   }
 }
